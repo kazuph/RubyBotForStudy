15ce1149af2c446e0caf570da28266dfc602bbe9
kazuhiro homma <kazu.homma@gmail.com>
2012-07-05 08:55:55 -0400

    TwitterBot作った！

--- bot.rb
+++ bot.rb
@@ -7,6 +7,15 @@ Twitter.configure do |config|
   config.oauth_token = '130326102-GBuxOMcO54I3dmvg0PnIuEL6bhny3rbovb8Hyrsb'
   config.oauth_token_secret = 'cqJhMIQmwN0OMUel4u8sgz7WVVFMwlZTQuiRmdnSa1w'
 end
-# Twitter.update(open('twitter.txt').readlines.shuffle.first)
-Twitter.update(ARGV[0])
-# p Twitter.home_timeline
+
+words = ["そんな実装で大丈夫か？", "お前が寝ている間にプログラミングしてる奴に勝てるのか？", "そんなコードで大丈夫か？", "m9( ･`д･´)プログラミングしろ！", "( ﾟДﾟ)ﾈﾑﾋｰ", "IE爆発しろ！", "テスト"];
+
+p word = words[(rand * words.length).to_i] + words[(rand * words.length).to_i]
+
+begin
+    Twitter.update("@kazuph " + word)
+    # Twitter.update(word)
+rescue => ex
+    puts "ERORR " + ex.message
+end
+

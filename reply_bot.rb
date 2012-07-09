865dd143fc5572893d231c1737ce990da33e2d4a
kazuhiro homma <kazu.homma@gmail.com>
2012-07-09 08:18:18 -0400

    とりあえずbotは例外処理を充実させて、リプライBOTは俺のタイムらいにコードがあったら何かリプルようにした

--- reply_bot.rb
+++ reply_bot.rb
@@ -4,7 +4,7 @@
 require "twitter"
 require "timeout"
 require "./config.rb"
-p $twitter_config
+require "pp"
 
 # 設定読み込み./config
 # twitter_config = Hash.new
@@ -21,26 +21,42 @@ Twitter.configure do |config|
 end
 
 # つぶやく単語セット
-words = ["そんな実装で大丈夫か？", "お前が寝ている間にプログラミングしてる奴に勝てるのか？", "そんなコードで大丈夫か？", "m9( ･`д･´)プログラミングしろ！", "( ﾟДﾟ)ﾈﾑﾋｰ", "IE爆発しろ！", "テスト"]
+words = ["テストコードが書け！", "お前が寝ている間にプログラミングしてる奴に勝てるのか？", "そんなコードで大丈夫か？", "m9( ･`д･´)プログラミングしろ！", "( ﾟДﾟ)ﾈﾑﾋｰ", "IE爆発しろ！", "テスト"]
 
-p word = words[(rand * words.length).to_i] + words[(rand * words.length).to_i]
+# p word = words[(rand * words.length).to_i] + words[(rand * words.length).to_i]
 
-# 投稿
+# タイムラインを取得して特定のワードに対して
 # 10秒でタイムアウト
 # トライは3回まで
 try_count = 0
 begin
   try_count += 1
-  timeout(10) { Twitter.update(user + word)}
-  # Twitter.update(word)
+  my_tweets = timeout(10) do
+    Twitter.user_timeline(user)
+    # Twitter.update(user + word)
+  end
+  # ツイートを検索
+  my_tweets.each do |tweet|
+    # とりあえずコードって呟いてたらリプル
+    if /コード/ =~ tweet.text
+      print tweet.text, "\n"
+      timeout(10) do
+        word = words[(rand * words.length).to_i] + words[(rand * words.length).to_i]
+        Twitter.update("@" + user + word + " " + tweet.id.to_s)
+      end
+    end
+  end
 rescue Timeout::Error => error
   # タイムアウトしたときに発動する
   puts "ERORR TIMEOUT " + error.message
+  if try_count < 3
+    sleep 1
+    retry
+  end
 rescue => error
   # なにかのエラーで発動する
   puts "ERORR " + error.message
   if try_count < 3
-    p word = words[(rand * words.length).to_i] + words[(rand * words.length).to_i]
     sleep 1
     retry
   end
